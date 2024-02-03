420a94fc7200cd911557b2584e8ac88eaf81591e
Aditya Joshi <aditya03joshi@gmail.com>
2024-02-03 23:32:34 +0530

    lc update-3

---
+++ src/string/Leetcode/OddNumInString.java
@@ -0,0 +1,19 @@
+package string.Leetcode;
+
+public class OddNumInString {
+    public static void main(String[] args) {
+        String s = "523" ;
+        System.out.println(largestOddNumber(s));
+
+    }
+    static String largestOddNumber(String num) {
+        for (int i = num.length()-1; i>=0; i--) {
+            if(Character.getNumericValue(num.charAt(i))%2 !=0 ) {
+                return num.substring(0,i+1);
+            }
+
+        }
+        return  "";
+    }
+    }
+
